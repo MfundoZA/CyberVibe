d1ff5fb0c79004f4db7eb146b6bc9e61f3d32dac
MfundoZA <20117271@vcconnect.co.za>
2021-06-30 15:50:40 +0200

    Create project models

--- ZMusic.Core/Models/Album.cs
+++ ZMusic.Core/Models/Album.cs
@@ -7,6 +7,11 @@ namespace ZMusic.Core.Models
 {
     class Album : INotifyPropertyChanged
     {
+        private string title;
+        private string artist;
+        private int numberOfTracks;
+        private int yearRealeased;
+        private Genre genre;
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string propertyName)
---
+++ ZMusic.Core/Models/Genre.cs
@@ -0,0 +1,7 @@
+﻿namespace ZMusic.Core.Models
+{
+    public enum Genre
+    {
+        Rap, HipHop, Pop, Jazz, Blues, Electronic, Rock, Punk, Country, House, Metal
+    }
+}
